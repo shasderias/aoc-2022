71bb9f8ab291ea1c5135cf73774009f6d7696e06
Lim Yue Chuan <shasderias@gmail.com>
2022-12-08 16:51:05 +0800

    day3-rust

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+use itertools::Itertools;
+
 const INPUT_FILE_PATH: &str = "input.txt";
 
 fn main() {
-    star2()
+    star1();
+    star2();
 }
 
 fn star1() {
@@ -30,21 +33,24 @@ fn star1() {
 fn star2() {
     let file = File::open(INPUT_FILE_PATH)
         .expect("error opening file");
-    let lines = io::BufReader::new(file);
+    let lines = io::BufReader::new(file).lines();
 
     let mut total_value = 0;
 
 
-    for group_lines in lines.chunks(3) {
-        println!("{group_lines}");
-        // let line = line.unwrap();
-        // let len = line.len() / 2;
-        // let comp1 = line[..len].to_string();
-        // let comp2 = line[len..].to_string();
-        //
-        // let common = intersect(&comp1, &comp2).unwrap_or('!');
-        // let iv = item_value(common);
-        // total_value += iv;
+    for (line1, line2, line3) in lines.tuples() {
+        let line1 = line1.unwrap();
+        let line2 = line2.unwrap();
+        let line3 = line3.unwrap();
+
+        let line12common = intersect2(&line1, &line2);
+        let badge = intersect2(&line12common, &line3);
+        if badge.len() != 1 {
+            println!("{:?}", badge);
+            return
+        }
+        let iv = item_value(badge.chars().next().unwrap());
+        total_value += iv;
     }
     println!("{total_value}");
 }
@@ -58,10 +64,10 @@ fn intersect(a: &str, b: &str) -> Option<char> {
     return None;
 }
 
-fn intersect2(a: &str, b: &str) -> String {
+fn intersect2(a: &str, b: &str) -> String{
     let mut ic = String::new();
     for c in a.chars() {
-        if b.contains(c) {
+        if b.contains(c) && !ic.contains(c) {
             ic.push(c);
         }
     }
